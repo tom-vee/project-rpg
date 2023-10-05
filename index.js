6240f2c24762c74bb5103b5c400bae0a0d0cbd34
Tom Vaughan <146029631+tom-vee@users.noreply.github.com>
2023-10-05 13:18:55 +0100

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let health = 10;
 let maxHealth = 10;
 
 // Weapon Stats
-let weaponDamage = 2;
+let weaponDamage = 25;
 
 // Viper Stats
 let viperAttack = 10;
@@ -16,13 +16,15 @@ let viperHealth = 20;
 
 //Inventory
 let healPotions = 3;
-let coin = 10;
+let coin = 2;
 let weapon = 'Dagger';
 
 //Store
-let epicSwordStockText = '1 in stock - Buy';
-let swordStockText = '1 in stock - Buy'
-let healPotionStockText = '5 in stock - Buy'
+let healPotionStockNum = 5;
+let epicSwordStockNum = 1;
+let swordStockNum = 1;
+
+
 
 function heroHeal() {
     if ( healPotions > 0) {
@@ -41,6 +43,9 @@ function heroHeal() {
 function xpUp() {
     xp += 10;
     document.getElementById("xpText").innerText= xp;
+    maxHealth += 10;
+    health = maxHealth;
+    document.getElementById("healthText").innerText = health;
     levelUp();
 }
 
@@ -48,40 +53,43 @@ function levelUp() {
     switch (xp){
     case 10:
         level += 1;
-        alert("Congrats! You're level 1.\n+2 Attack\n+2 Defence\n+10 Health");
+        alert("Congrats! You're level 1.\n+2 Attack\n+2 Defence\n+10 Health\n\n30 coin bonus!");
         document.getElementById("levelText").innerText = level;
         attack += 2;
         document.getElementById("attackText").innerText = attack;
         defence += 2;
         document.getElementById("defenceText").innerText = defence;
-        maxHealth += 10;
+        coin += 30;
+        document.getElementById("coinText").innerText = coin;
         break;
-    case 30:
+    case 50:
         level += 1;
-        alert("Congrats! You're level 2.\n+2 Attack\n+2 Defence\n+10 Health");
+        alert("Congrats! You're level 2.\n+2 Attack\n+2 Defence\n+10 Health\n\n30 coin bonus!");
         document.getElementById("levelText").innerText = level;
         attack += 2;
         document.getElementById("attackText").innerText = attack;
         defence += 2;
         document.getElementById("defenceText").innerText = defence;
-        maxHealth += 10;
+        coin += 30;
+        document.getElementById("coinText").innerText = coin;
         break;
-    case 60:
+    case 100:
         level += 1;
-        alert("Congrats! You're level 3.\n+2 Attack\n+2 Defence\n+10 Health");
+        alert("Congrats! You're level 3.\n+2 Attack\n+2 Defence\n+10 Health\n\n30 coin bonus!");
         document.getElementById("levelText").innerText = level;
         attack += 2;
         document.getElementById("attackText").innerText = attack;
         defence += 2;
         document.getElementById("defenceText").innerText = defence;
-        maxHealth += 10;
+        coin += 30;
+        document.getElementById("coinText").innerText = coin;
         break;
     }
 }
 
 function viperDeath() {
     if (viperHealth <=0){
-        alert("Viper Defeated! - 10XP gained\nYou looted 10 coins and 3 Heal Potions!");
+        alert("Viper Defeated! - 10XP gained\nYou looted coins and Heal Potions!");
         viperHealth = 30;
         document.getElementById("viperHealthText").innerText = viperHealth;
         healPotions += Math.floor(Math.random()*3);
@@ -93,7 +101,7 @@ function viperDeath() {
 }
 function dead() {
 if (health < 1){
-    alert("Game Over\nYou're dead");
+    alert("Game Over\nYou're dead\nRefresh the page to replay.");
 }
 }
 /*
@@ -101,7 +109,7 @@ if (health < 1){
 - Percentage to hit: (hit * attack)/100
 */
 function viperDamage() {
-    var hit = Math.floor(attack / weaponDamage);
+    var hit = Math.floor(attack * weaponDamage);
     var percHit = Math.floor(hit * attack * weaponDamage);
     var per = Math.floor(Math.random() * 101);
     //console.log(hit);
@@ -137,34 +145,51 @@ function heroDamage() {
 }
 
 function buySword() {
-    if (coin >= 10){
+    if (coin >= 100 && swordStockNum > 0){
         weapon = 'Sword';
         document.getElementById("weaponText").innerText = weapon;
-        weaponDamage = 10;
+        weaponDamage += 5;
         document.getElementById("weaponDamageText").innerText = weaponDamage;
-        coin -= 10;
+        coin -= 100;
+        document.getElementById("coinText").innerText = coin;
+        swordStockNum -= 1;
+        document.getElementById("swordStockText").innerText = swordStockNum;
+        alert('You bought a Sword!')
+    } else if (coin >= 100 && swordStockNum == 0  ){
+        alert('The store is out of Swords')
     } else {
         alert('You do not have enough money!\nGet out of the store!')
     }
 }
 
 function buyEpicSword() {
-    if (coin >= 100){
+    if (coin >= 250 && epicSwordStockNum > 0){
         weapon = 'Epic Sword';
         document.getElementById("weaponText").innerText = weapon;
-        weaponDamage = 25;
+        weaponDamage += 15;
         document.getElementById("weaponDamageText").innerText = weaponDamage;
-        coin -= 100;
+        coin -= 250;
+        document.getElementById("coinText").innerText = coin;
+        epicSwordStockNum -= 1;
+        document.getElementById("epicSwordStockText").innerText = epicSwordStockNum;
+        alert('You bought an Epic Sword!')
+    } else if (coin >= 250 && epicSwordStockNum == 0  ){
+        alert('The store is out of Epic Swords')
     } else {
         alert('You do not have enough money!\nGet out of the store!')
     }
 }
 
 function buyPotion() {
-    if (coin >= 2){
+    if (coin >= 2 && healPotionStockNum > 0){
         healPotions += 1;
         document.getElementById("healPotionText").innerText = healPotions;
         coin -= 2;
+        document.getElementById("coinText").innerText = coin;
+        healPotionStockNum -= 1;
+        document.getElementById("healPotionStockText").innerText = healPotionStockNum;
+    } else if (coin >= 2 && healPotionStockNum === 0  ){
+        alert('The store is out of Heal Potions')
     } else {
         alert('You do not have enough money!\nGet out of the store!')
     }
